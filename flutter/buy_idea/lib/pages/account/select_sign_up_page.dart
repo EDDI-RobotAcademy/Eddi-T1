9d7ea9dfd34a9672152c00eaab175be3ed9b1707
G <shjwin96@gmail.com>
2022-12-01 22:27:51 +0900

    feat: [진건오] 회원가입을 하려는 사용자가 목적에 맞는 회원가입 페이지에 위치하는 로고를 통해 메인페이지로 이동할 수 있는 UI[DIY-SALES-APP-UI 9]

--- flutter/buy_idea/lib/pages/account/select_sign_up_page.dart
+++ flutter/buy_idea/lib/pages/account/select_sign_up_page.dart
@@ -1,6 +1,9 @@
 import 'package:buy_idea/component/account/select_sign_up_form.dart';
+import 'package:buy_idea/component/logo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../utility/size.dart';
 
 class SelectSignUpPage extends StatelessWidget{
   const SelectSignUpPage({Key? key}) : super(key: key);
@@ -8,11 +11,14 @@ class SelectSignUpPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
           children: [
-            SelectSignUpForm()
+            SizedBox(height: 220),
+            Logo(),
+            SizedBox(width: large_gap, height: 90.0),
+            SelectSignUpForm(),
           ],
         ),
       ),
