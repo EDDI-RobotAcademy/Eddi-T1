be0c62050a44abe15a06a111b179d859ffa6581a
joanna930224 <annavu0224@gmail.com>
2022-12-02 18:07:55 +0900

    feat:[조안나] 사용자가 목적에 맞는 회원가입을 하기 위해 판매자/일반회원 모드를 선택하는 페이지 UI 추가 수정 [DIY-SALES-WEB-UI 1]

--- vue/frontend/src/components/account/SignUpChoiceForm.vue
+++ vue/frontend/src/components/account/SignUpChoiceForm.vue
@@ -3,13 +3,13 @@
     <div class="white" style="font-family: Arial">
       <v-col cols="auto" style="padding-bottom: 90px">
         <v-img
-            :src="require('@/assets/buydia_logo.png')" width="300" class="mx-auto mb-6"/>
+            :src="require('@/assets/sign_up_choice.png')" width="800" class="mx-auto mb-6"/>
         <h5 class="text-md-center" style="font-weight: normal">
           회원 타입을 선택해주세요.</h5>
         <v-row justify="center">
-          <div class="my-2">
-            <v-btn width="200" height="150" text large outlined style="font-size: large"
-                   class="mx-auto" id="heightSizeBox" color="#2F4F4F" @click="null">
+          <div style="margin-right: 30px">
+            <v-btn width="200" height="150" text large outlined style
+                   class="mx-auto" id="signUpButton" color="#2F4F4F" @click="null">
               <h5 style="font-weight: normal">
                 <v-icon size="50px">mdi-account-plus-outline</v-icon>
                 <br/> 판매자 회원가입
@@ -17,11 +17,8 @@
             </v-btn>
           </div>
           <div>
-            <v-container></v-container>
-          </div>
-          <div class="my-2">
-            <v-btn width="200" height="150" text large outlined style="font-size: large"
-                   class="mx-auto" id="heightSizeBox" color="#2F4F4F" @click="null">
+            <v-btn width="200" height="150" text large outlined style
+                   class="mx-auto" id="signUpButton" color="#2F4F4F" @click="null">
               <h5 style="font-weight: normal">
                 <v-icon size="50px">mdi-account-plus</v-icon>
                 <br/> 일반회원 회원가입
@@ -29,7 +26,7 @@
             </v-btn>
           </div>
         </v-row>
-        <div id="heightSizeBox" class="text-md-center">
+        <div id="signUpButton" class="text-md-center">
           <h5 style="font-weight: normal">
             이미 가입한 계정이 있으신가요?</h5>
           <router-link to="/sign-in-view"
@@ -49,8 +46,9 @@ export default {
 </script>
 
 <style scoped>
-#heightSizeBox {
+#signUpButton {
   margin-top: 50px;
+  font-size: large;
 }
 
 </style>
