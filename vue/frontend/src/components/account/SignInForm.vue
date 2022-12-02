3ff24d8391a584653950ef61e785bfee6c091a3c
parkseonho1 <preferr1@naver.com>
2022-12-02 21:43:27 +0900

    feat: [박선호] 로그인하기 위한 로그인 페이지 UI [DIY-SALES-WEB-UI 7]

---
+++ vue/frontend/src/components/account/SignInForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container style="background-color: red">
+    <v-row justify="center" style="margin-top: 200px">
+      <v-card>dd</v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "SignInForm"
+}
+</script>
+
+<style scoped>
+
+</style>
