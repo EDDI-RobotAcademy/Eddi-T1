46776171c1ded941ef65c59852399853c8d03852
joanna930224 <annavu0224@gmail.com>
2022-12-02 21:29:31 +0900

    feat:[조안나] 판매자용 모드를 선택한 사용자가 사이트에 가입하기 위한 회원가입 페이지 UI [DIY-SALES-WEB-UI 4]

--- vue/frontend/src/components/account/SignUpChoiceForm.vue
+++ vue/frontend/src/components/account/SignUpChoiceForm.vue
@@ -4,17 +4,21 @@
       <v-col cols="auto" style="padding-bottom: 90px">
         <v-img
             :src="require('@/assets/sign_up_choice.png')" width="800" class="mx-auto mb-6"/>
+        <v-img
+            :src="require('@/assets/sign_up_choice_num1.png')" width="150" class="mx-auto mb-6"/>
         <h5 class="text-md-center" style="font-weight: normal">
           회원 타입을 선택해주세요.</h5>
         <v-row justify="center">
           <div style="margin-right: 30px">
-            <v-btn width="200" height="150" text large outlined style
-                   class="mx-auto" id="signUpButton" color="#2F4F4F" @click="null">
-              <h5 style="font-weight: normal">
-                <v-icon size="50px">mdi-account-plus-outline</v-icon>
-                <br/> 판매자 회원가입
-              </h5>
-            </v-btn>
+            <router-link :to="{name: 'SellerSignUpView'}" style="text-decoration: none">
+              <v-btn width="200" height="150" text large outlined style
+                     class="mx-auto" id="signUpButton" color="#2F4F4F">
+                <h5 style="font-weight: normal">
+                  <v-icon size="50px">mdi-account-plus-outline</v-icon>
+                  <br/> 판매자 회원가입
+                </h5>
+              </v-btn>
+            </router-link>
           </div>
           <div>
             <v-btn width="200" height="150" text large outlined style
--- vue/frontend/src/router/index.js
+++ vue/frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUpChoiceView from "@/views/account/SignUpChoiceView";
+import SellerSignUpView from "@/views/account/SellerSignUpView";
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/sign-up-choice',
     name: 'SignUpChoiceView',
     component: SignUpChoiceView
+  },
+  {
+    path: '/seller-sign-up',
+    name: 'SellerSignUpView',
+    component: SellerSignUpView
   }
 ]
 
