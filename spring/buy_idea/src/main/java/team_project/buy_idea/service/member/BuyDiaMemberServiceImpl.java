54a0bbacc20e125bea0b2f3db86d9f2c62e05853
Jaebeom Im <ljb8194@gmail.com>
2022-12-06 21:06:10 +0900

    feat:[임재범] 회원가입시 비밀번호 암호화하여 저장하는 시스템 [DIY-SALES PROCESS 4]

--- spring/buy_idea/src/main/java/team_project/buy_idea/service/member/BuyDiaMemberServiceImpl.java
+++ spring/buy_idea/src/main/java/team_project/buy_idea/service/member/BuyDiaMemberServiceImpl.java
@@ -3,7 +3,10 @@ package team_project.buy_idea.service.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import team_project.buy_idea.entity.member.Authentication;
+import team_project.buy_idea.entity.member.BasicAuthentication;
 import team_project.buy_idea.entity.member.BuyDiaMember;
+import team_project.buy_idea.repository.member.AuthenticationRepository;
 import team_project.buy_idea.repository.member.BuyDiaMemberRepository;
 import team_project.buy_idea.service.member.request.BuyDiaMemberRegisterRequest;
 
@@ -14,7 +17,8 @@ public class BuyDiaMemberServiceImpl implements BuyDiaMemberService {
     @Autowired
     private BuyDiaMemberRepository buyDiaMemberRepository;
 
-
+    @Autowired
+    private AuthenticationRepository authenticationRepository;
 
 
     @Override
@@ -22,6 +26,10 @@ public class BuyDiaMemberServiceImpl implements BuyDiaMemberService {
         final BuyDiaMember member = request.toMember();
         buyDiaMemberRepository.save(member);
 
+        final BasicAuthentication auth = new BasicAuthentication(member,
+                Authentication.BASIC_AUTH, request.getPassword());
+
+        authenticationRepository.save(auth);
 
         return true;
     }
---
+++ spring/buy_idea/src/main/java/team_project/buy_idea/utility/password/PasswordHashConverter.java
@@ -0,0 +1,21 @@
+package team_project.buy_idea.utility.password;
+
+
+import team_project.buy_idea.utility.encrypt.EncryptionUtil;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class PasswordHashConverter implements AttributeConverter<String, String> {
+
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        return EncryptionUtil.generateHash(attribute);
+    }
+
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        return dbData;
+    }
+}
