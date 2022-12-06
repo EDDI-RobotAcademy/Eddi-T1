2931d452bafdbe0879171120226ecfe6ad725383
parkseonho1 <preferr1@naver.com>
2022-12-06 21:18:03 +0900

    feat: [박선호] 사용자가 로그인 버튼을 눌렀을 때 서버로 로그인 요청을 보내는 시스템 [DIY-SALES-WEB-UI 12]

--- vue/frontend/src/views/account/SignInView.vue
+++ vue/frontend/src/views/account/SignInView.vue
@@ -1,15 +1,21 @@
 <template>
   <v-container>
-    <sign-in-form/>
+    <sign-in-form @submit="onSubmit"/>
   </v-container>
 </template>
 
 <script>
 import SignInForm from "@/components/account/SignInForm";
+import {mapActions} from "vuex";
 export default {
   name: "SignInView",
   components: {SignInForm},
   methods: {
+    ...mapActions(['requestBuyerSignInToSpring']),
+    async onSubmit(payload) {
+      const { memberId, password } = payload
+      await this.requestBuyerSignInToSpring({ memberId, password })
+    }
   }
 }
 </script>
