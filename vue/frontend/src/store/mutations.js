d4666c115b67dc30aa0a63a54905f4e85262b041
parkseonho1 <preferr1@naver.com>
2022-12-06 21:18:03 +0900

    feat: [박선호] 사용자가 로그인 버튼을 눌렀을 때 서버로 로그인 요청을 보내는 시스템 [DIY-SALES-WEB-UI 12]

--- vue/frontend/src/store/mutations.js
+++ vue/frontend/src/store/mutations.js
@@ -1,10 +1,13 @@
 import {
     REQUEST_ID_PASS_CHECK,
-
+    REQUEST_SIGN_IN_TOKEN_FROM_SPRING
 } from './mutation-types'
 
 export default {
     [REQUEST_ID_PASS_CHECK](state, passingData) {
         state.unDuplicateIdPass = passingData
+    },
+    [REQUEST_SIGN_IN_TOKEN_FROM_SPRING] (state, passingData) {
+        state.userToken = passingData
     }
 }
