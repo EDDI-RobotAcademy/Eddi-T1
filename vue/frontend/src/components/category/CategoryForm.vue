1fe2877fe07e3453db135cdebc7b1db0e78392e8
G <shjwin96@gmail.com>
2022-12-09 17:28:13 +0900

    feat: [진건오] 로그인 유무를 떠나 모든 사용자가 페이지 접속시 보여지는 메인페이지 UI [DIY-SALES-WEB-UI 17]

---
+++ vue/frontend/src/components/category/CategoryForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container style="width: 1400px">
+    <v-row style="margin-top: 30px">
+      <v-layout>
+        <div style="margin-top: 30px"
+
+        >
+          <h2 style="font-weight: bold">{{ categoryName }}</h2>
+        </div>
+      </v-layout>
+      <v-spacer></v-spacer>
+
+      <div style="margin-top: 30px; margin-bottom: 30px">
+        <span
+            class="mr-2
+            grey--text"
+        >
+            {{ page }} / {{ null }}
+          </span>
+        <v-btn
+            fab
+            dark
+            x-small
+            style="background-color: #2F4F4F"
+            elevation="0"
+            class="mr-1"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn
+            fab
+            dark
+            style="background-color: #2F4F4F"
+            x-small
+            elevation="0"
+            class="ml-1"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
+    </v-row>
+    <v-divider></v-divider>
+
+    <v-container>
+      <v-layout>
+        <v-row class="justify-center">
+          <v-card v-for="(item, index) in categoryItems" :key="index"
+                  class="ma-4"
+                  max-width="210"
+          >
+            <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="require('@/assets/productImg/'+item.img)"
+            >
+            </v-img>
+            <v-card-subtitle class="pb-0">
+              {{ item.sellerNickname }}
+            </v-card-subtitle>
+            <v-card-title>
+              <h4>{{ item.commodityTitle }}</h4>
+            </v-card-title>
+            <v-divider></v-divider>
+
+            <v-card-text class="text--primary">
+              {{ item.contents }}
+            </v-card-text>
+
+          </v-card>
+        </v-row>
+      </v-layout>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CategoryForm",
+  props:{
+    categoryName:{
+      type: String
+    },
+    categoryItems:{
+      type: Array
+    }
+  },
+  data(){
+    return {
+      page: 1,
+      itemsPerPage: 5,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
