d6dc67f9268ac87d5fae9d300eb3bd495949957f
ANNA <109578911+joanna930224@users.noreply.github.com>
2022-12-13 01:29:23 +0900

    feat: [조안나] 일반회원/비회원이 bottom Navigation bar에서 ‘카테고리’를 탭했을 때 나타나는 페이지 UI [DIY-SALES-APP-UI 23]

--- flutter/buy_idea/lib/pages/buyer/category/handmade_page.dart
+++ flutter/buy_idea/lib/pages/buyer/category/handmade_page.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../component/buyer/app_bar/top_bar.dart';
+import '../../../component/buyer/category/handmade_form.dart';
 
 class HandmadePage extends StatefulWidget {
   const HandmadePage({Key? key}) : super(key: key);
@@ -10,8 +13,13 @@ class HandmadePage extends StatefulWidget {
 class _HandmadePageState extends State<HandmadePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("핸드메이드 임시 페이지", style: TextStyle(fontSize: 20),),
-    );
+    return Scaffold(
+        appBar: TopBar(),
+        body: Column(
+          children: [
+            HandmadeForm(),
+            Text("핸드메이드 임시 페이지", style: TextStyle(fontSize: 20)),
+          ],
+        ));
   }
 }
