9597fe8a1e1f29ed2c2423484288242e0fdff306
parkseonho1 <preferr1@naver.com>
2022-12-19 18:38:18 +0900

    feat: [박선호] 상품에 관한 정보, 후기, 문의 항목 중 선택해서 볼 수 있는 Tab Bar UI [DIY-SALES-APP-UI 30]

--- flutter/buy_idea/lib/pages/buyer/category/product/product_details_page.dart
+++ flutter/buy_idea/lib/pages/buyer/category/product/product_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:buy_idea/component/buyer/category/product/product_details_page_header.dart';
 import 'package:buy_idea/component/buyer/category/product/product_details_page_image_view.dart';
+import 'package:buy_idea/component/buyer/category/product/product_info_form.dart';
+import 'package:buy_idea/component/buyer/category/product/product_buy_and_shopping_cart_select_modal_sheet.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailsPage extends StatefulWidget {
@@ -10,6 +12,8 @@ class ProductDetailsPage extends StatefulWidget {
 }
 
 class _ProductDetailsPageState extends State<ProductDetailsPage> with TickerProviderStateMixin {
+
+  late TabController tabController;
 
   final List<String> imageList = [
     'assets/product/handmade1.jpg',
@@ -26,6 +30,22 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> with TickerProv
   final int productPrice = 28600;
   final int shippingCost = 2500;
   final int freeShippingCost = 50000;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  Widget _tabMenu(String menu) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Text(
+          menu,
+          style: TextStyle(fontSize: 13, color: Colors.black)
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,13 +78,66 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> with TickerProv
             SliverToBoxAdapter(
                 child: SizedBox(height: 20)
             ),
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: TabBarDelegate(
+                  widget: Container(
+                    height: AppBar().preferredSize.height,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        border: Border(
+                            bottom: BorderSide(color: Colors.black12),
+                            top: BorderSide(color: Colors.black12)
+                        )
+                    ),
+                    child: TabBar(
+                      controller: tabController,
+                      indicatorColor: Color(0xff2F4F4F),
+                      tabs: [
+                        _tabMenu('상품 정보'),
+                        _tabMenu('구매 후기'),
+                        _tabMenu('문의')
+                      ],
+                    ),
+                  )
+              ),
+            ),
           ];
         },
         body: Padding(
           padding: EdgeInsets.only(bottom: 60),
-          child: Container(),
+          child: TabBarView(
+              controller: tabController,
+              children: [
+                Text('상품 정보'),
+                Text('구매 후기'),
+                Text('문의'),
+              ]
+          ),
         ),
       ),
     );
+  }
+}
+
+class TabBarDelegate extends SliverPersistentHeaderDelegate {
+  TabBarDelegate({required this.widget});
+  Widget widget;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return widget;
+  }
+
+  @override
+  double get maxExtent => AppBar().preferredSize.height;
+
+  @override
+  double get minExtent => AppBar().preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
