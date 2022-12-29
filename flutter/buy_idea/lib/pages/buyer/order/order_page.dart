05296d8add0164d27853480d781ecbbef82b2c73
parkseonho1 <preferr1@naver.com>
2022-12-29 18:38:26 +0900

    feat: [박선호] 주문 페이지의 배송지 입력 UI [DIY-SALES-APP-UI 59]

--- flutter/buy_idea/lib/pages/buyer/order/order_page.dart
+++ flutter/buy_idea/lib/pages/buyer/order/order_page.dart
@@ -1,4 +1,5 @@
 import 'package:buy_idea/component/buyer/app_bar/title_top_bar.dart';
+import 'package:buy_idea/component/buyer/order/delivery_address_input_form.dart';
 import 'package:buy_idea/component/buyer/order/ordering_customer_info_form.dart';
 import 'package:flutter/material.dart';
 
@@ -10,19 +11,39 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController recipientController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController zipcodeController = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+  final TextEditingController streetController = TextEditingController();
+  final TextEditingController addressDetailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TitleTopBar(titleText: '상품 결제'),
       body: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          color: Colors.grey,
-          child: Column(
-            children: [
-              OrderingCustomerInfoForm(),
-              SizedBox(height: 10),
-            ],
+        child: Form(
+          key: _formKey,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.grey,
+            child: Column(
+              children: [
+                OrderingCustomerInfoForm(),
+                SizedBox(height: 10),
+                DeliveryAddressInputForm(
+                  recipientController: recipientController,
+                  phoneController: phoneController,
+                  zipcodeController: zipcodeController,
+                  cityController: cityController,
+                  streetController: streetController,
+                  addressDetailController: addressDetailController,
+                )
+              ],
+            ),
           ),
         ),
       ),
