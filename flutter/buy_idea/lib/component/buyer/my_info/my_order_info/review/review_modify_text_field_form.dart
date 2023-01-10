2440169a38a5b6c508670b320bbd6524223bd12e
parkseonho1 <preferr1@naver.com>
2023-01-11 01:11:06 +0900

    feat: [박선호] 상품 구매 후기 수정 페이지 UI [DIY-SALES-APP-UI 92]

---
+++ flutter/buy_idea/lib/component/buyer/my_info/my_order_info/review/review_modify_text_field_form.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ReviewModifyTextFieldForm extends StatefulWidget {
+  final String nickname;
+  final TextEditingController contentController;
+  const ReviewModifyTextFieldForm({
+    Key? key,
+    required this.nickname,
+    required this.contentController
+  }) : super(key: key);
+
+  @override
+  State<ReviewModifyTextFieldForm> createState() => _ReviewModifyTextFieldFormState();
+}
+
+class _ReviewModifyTextFieldFormState extends State<ReviewModifyTextFieldForm> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Divider(height: 15, color: Colors.grey, thickness: 1),
+          Row(
+            children: [
+              SizedBox(
+                width: 70,
+                child: Text(
+                  '닉네임',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ),
+              Expanded(
+                child: Text(
+                  widget.nickname,
+                  style: TextStyle(fontSize: 13),
+                  textAlign: TextAlign.center,
+                )
+              )
+            ],
+          ),
+          Divider(height: 15, color: Colors.grey, thickness: 1),
+          SizedBox(height: 10),
+          TextFormField(
+            maxLines: 15,
+            controller: widget.contentController,
+            style: TextStyle(fontSize: 13),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black)
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 2.0)
+              ),
+              label: Icon(Icons.edit),
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              hintText: '구매 후기 내용을 작성해주세요...',
+              hintStyle: TextStyle(fontSize: 13),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
