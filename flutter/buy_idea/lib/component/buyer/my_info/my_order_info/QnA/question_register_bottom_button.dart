a35ae650432f1f5a687851da03becde099e76287
ANNA <109578911+joanna930224@users.noreply.github.com>
2023-01-12 17:49:04 +0900

    feat: [조안나] 문의글 등록 완료시 해당 상품 상세페이지로 이동하는 시스템 [DIY-SALES-APP-UI 101]

--- flutter/buy_idea/lib/component/buyer/my_info/my_order_info/QnA/question_register_bottom_button.dart
+++ flutter/buy_idea/lib/component/buyer/my_info/my_order_info/QnA/question_register_bottom_button.dart
@@ -2,7 +2,9 @@ import 'package:buy_idea/api/spring_qna_api.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class QuestionRegisterBottomButton extends StatelessWidget {
+import '../../../../../pages/buyer/product/product_details_page.dart';
+
+class QuestionRegisterBottomButton extends StatefulWidget {
   const QuestionRegisterBottomButton(
       {Key? key,
       required this.contentController,
@@ -11,7 +13,8 @@ class QuestionRegisterBottomButton extends StatelessWidget {
       required this.questionCategory,
       required this.questionTitle,
       required this.openStatus,
-      required this.questionContent})
+      required this.questionContent,
+      })
       : super(key: key);
   final TextEditingController contentController;
   final int productNo;
@@ -22,13 +25,18 @@ class QuestionRegisterBottomButton extends StatelessWidget {
   final bool openStatus;
 
   @override
+  State<QuestionRegisterBottomButton> createState() => _QuestionRegisterBottomButtonState();
+}
+
+class _QuestionRegisterBottomButtonState extends State<QuestionRegisterBottomButton> {
+  @override
   Widget build(BuildContext context) {
     return ElevatedButton(
         onPressed: () {
-          if (contentController.text.isNotEmpty) {
+          if (widget.contentController.text.isNotEmpty) {
             ///spring API 요청
-            Question question = Question(productNo, writer, questionCategory,
-                questionTitle, questionContent, openStatus);
+            Question question = Question(widget.productNo, widget.writer, widget.questionCategory,
+                widget.questionTitle, widget.questionContent, widget.openStatus);
             debugPrint('question : ' + question.toString());
 
             var questionRegister = SpringQnaApi().questionRegister(question);
@@ -50,7 +58,8 @@ class QuestionRegisterBottomButton extends StatelessWidget {
                           TextButton(
                               onPressed: () {
                                 Get.back();
-                                Get.back();
+                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProductDetailsPage(productNo: widget.productNo)))
+                                    .then((value) => setState(() {}));
                               },
                               child: Text('확인'))
                         ],
