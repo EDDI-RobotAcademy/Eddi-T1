d1368a3785c79b9b2d362dc0b883c8fb9319c259
ANNA <109578911+joanna930224@users.noreply.github.com>
2023-01-14 00:48:09 +0900

    feat: [조안나] 판매자가 등록한 상품의 답변 상태에 따른 문의내역을 요청하는 API [DIY-SALES-APP-UI 107]

---
+++ flutter/buy_idea/lib/api/seller/spring_seller_qna_api.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../../component/buyer/my_info/my_order_info/QnA/my_question_history_info.dart';
+
+class SpringSellerQnaApi {
+  static const String httpUri = '192.168.0.8:8888';
+
+
+  Future<List<MyQuestionHistoryInfo>> getAnswerStatusList(AnswerStatusRequest request) async {
+    debugPrint('getAnswerStatusList()');
+    var body = json.encode(request);
+
+    var response = await http.post(
+        Uri.http(httpUri, '/qna/answer-status-list'),
+        headers: {"Content-Type": "application/json"},
+        body: body);
+
+    var data = jsonDecode(utf8.decode(response.bodyBytes)) as List;
+
+    if (response.statusCode == 200) {
+      List<MyQuestionHistoryInfo> quaList =
+      data.map((list) => MyQuestionHistoryInfo.fromJson(list)).toList();
+
+      debugPrint('통신 확인');
+      return quaList;
+    } else {
+      throw Exception('orderRegister() 에러 발생');
+    }
+  }
+
+}
+
+class AnswerStatusRequest{
+  String nickname;
+  String answerStatus;
+
+  AnswerStatusRequest(this.nickname, this.answerStatus);
+
+Map<String, dynamic> toJson() => {
+  'nickname': nickname,
+  'answerStatus': answerStatus,
+};
+}
+
+class Question {
+  int productNo;
+  String writer;
+  String questionCategory;
+  String questionTitle;
+  String questionContent;
+  bool openStatus;
+
+  Question(this.productNo, this.writer, this.questionCategory,
+      this.questionTitle, this.questionContent, this.openStatus);
+
+  Map<String, dynamic> toJson() => {
+    'productNo': productNo,
+    'writer': writer,
+    'questionCategory': questionCategory,
+    'questionTitle': questionTitle,
+    'questionContent': questionContent,
+    'openStatus': openStatus
+  };
+}
