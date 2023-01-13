32a6af08171c06b61362863701d4094d25069de8
Kim Soyeon <109579048+soyeonkim96@users.noreply.github.com>
2023-01-14 05:30:13 +0900

    feat: [김소연] 모든 사용자가 상품상세페이지의 리뷰를 클릭했을 때 해당 상품의 리뷰를 UI에 매핑하는 시스템[DIY-SALES-WEB-UI 69]

--- vue/frontend/src/components/product/review/ReviewForm.vue
+++ vue/frontend/src/components/product/review/ReviewForm.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
+<!--    총 개수 : {{ productReviewCnt }}-->
+
     <v-card flat style="margin-left: 25px; margin-top: 20px" width="95%">
 
       <v-layout
-          v-for="(itemList, index) in productReviewList" :key="index"
-          @page-count="pageCount = $event"
+          v-for="(itemList, index) in paginatedOrders" :key="index"
       >
 
         <v-card max-width="230px"
@@ -42,63 +43,15 @@
             {{ itemList.regDate | date }}
           </v-card-subtitle>
 
-
           <v-card-title style="padding-top: 0px; font-weight: normal; font-size: 16px; color: black">
             {{itemList.content}}
           </v-card-title>
         </v-card>
 
-      </v-layout>
-
-
-
-      <v-layout
-          v-for="(itemList, index) in productNextReviewList" :key="index"
-      >
-        <v-card max-width="230px"
-                style="padding: 25px 25px 25px 25px; border-bottom: 1px solid #eaebee;"
-                flat
-        >
-          <v-img
-              height="180px" flat color="#f5f5f5" style="border: 1px solid #d9d9d9; margin-top: 10px;"
-              :src="getReviewImg(index).reviewImgList">
-          </v-img>
-        </v-card>
-
-        <v-card width="100%"
-                flat
-                style="border-bottom: 1px solid #eaebee; margin-top: 15px"
-                tile
-        >
-          <v-card-title style="font-weight: bold; font-size: 15px">
-            <router-link to="null" style="text-decoration: none; color: black">
-              {{ itemList.writer }}
-            </router-link>
-
-            <v-spacer></v-spacer>
-            <v-rating
-                :value="itemList.starRating"
-                background-color="orange lighten-3"
-                color="orange"
-                small
-                dense hover readonly>
-            </v-rating>
-
-          </v-card-title>
-
-          <v-card-subtitle style="font-size: 12px; font-weight: bold">
-            {{ itemList.regDate | date }}
-          </v-card-subtitle>
-
-
-          <v-card-title style="padding-top: 0px; font-weight: normal; font-size: 16px; color: black">
-            {{itemList.content}}
-          </v-card-title>
-        </v-card>
       </v-layout>
 
       <v-btn
-          @click="addReview()"
+          @click="loadMore()"
           block
           large
           dense
@@ -108,23 +61,12 @@
           width="140px"
           elevation="0"
           style="color: #2F4F4F; margin-top: 10px; margin-right: 10px;"
+          v-if="currentPage * maxPerPage < productReviewList.length"
       >
-        더보기
+        더보기 <v-icon size="25" color="#2F4F4F">mdi-chevron-down</v-icon>
       </v-btn>
 
     </v-card>
-
-    <v-pagination
-        class="mt-10 mb-5"
-        v-model="page"
-        :length="pageCount"
-        total-visible="5"
-        color="#2F4F4F"
-        circle
-        flat
-    >
-    </v-pagination>
-
 
   </div>
 
@@ -132,6 +74,7 @@
 
 
 <script>
+import {mapActions, mapState} from "vuex";
 
 export default {
   name: "ReviewForm",
@@ -141,12 +84,35 @@ export default {
       required: true,
     },
   },
-
+  computed: {
+    ...mapState([
+        'productReviewList',
+        'productNextReviewList',
+        'reviewImage',
+        'productReviewCnt'
+    ]),
+    totalResults() {
+      return Object.keys(this.productReviewList).length;
+    },
+    pageCount() {
+      return Math.ceil(this.totalResults / this.maxPerPage);
+    },
+    pageOffest() {
+      return this.maxPerPage * this.currentPage;
+    },
+    paginatedOrders() {
+      return this.productReviewList.slice(0, this.currentPage * this.maxPerPage);
+    }
+  },
   data() {
     return {
       page: 1,
-      pageCount: 0,
+      /*pageCount: 0,*/
+      productReviewCnt: this.$store.state.productReviewCnt,
 
+      currentPage: 1,
+      maxPerPage: 3,
+      showReadMore: true
     }
   },
   filters: {
@@ -163,7 +129,12 @@ export default {
     }
   },
   methods: {
-
+    ...mapActions([
+        'requestProductReviewListFromSpring',
+       /* 'requestNextReviewListFromSpring',*/
+        'requestReviewImageFromSpring',
+        'requestReviewCntFromSpring',
+    ]),
     getReviewImg(index) {
      return {
        ...this.reviewImage,
@@ -171,13 +142,37 @@ export default {
      }
     },
 
+    loadMore() {
+      this.currentPage += 1;
+    },
   },
 
   async mounted() {
+    //리뷰 총 개수
+    const productNo = this.productNo
+    await this.requestReviewCntFromSpring(productNo)
+    console.log("productReviewCnt: " + this.productReviewCnt)
+
+
+    //리뷰리스트 불러오기
+    console.log("productNo: "+ this.productNo)
+    /*const productNo = this.productNo*/
+    const reviewSize = Number(this.productReviewCnt)
+
+    await this.requestProductReviewListFromSpring({productNo, reviewSize})
+
+
+    //쌓인 이미지 초기화
+    this.reviewImage.splice(0)
+    //리뷰 이미지 가져오기
+    for (let i = 0; i < this.productReviewList.length; i++) {
+      const reviewNo = this.productReviewList[i].reviewNo
+
+      await this.requestReviewImageFromSpring(reviewNo)
+    }
+    console.log("reviewImage: " + this.reviewImage)
 
   },
-
-
 
 }
 </script>
