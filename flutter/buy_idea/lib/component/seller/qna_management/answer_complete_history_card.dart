691c489205742b655495574df2228d2be571efbc
ANNA <109578911+joanna930224@users.noreply.github.com>
2023-01-14 00:47:19 +0900

    feat: [조안나] 판매자가 문의관리에서 답변완료를 탭했을 때 나타나는 문의내역 리스트 UI [DIY-SALES-APP-UI 108]

---
+++ flutter/buy_idea/lib/component/seller/qna_management/answer_complete_history_card.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AnswerCompleteHistoryCard extends StatefulWidget {
+  const AnswerCompleteHistoryCard(
+      {Key? key,
+      required this.productNo,
+      required this.title,
+      required this.writer,
+      required this.nickname,
+      required this.questionCategory,
+      required this.questionTitle,
+      required this.questionContent,
+      required this.answer,
+      required this.answerStatus,
+      required this.regDate,
+      required this.updDate,
+      required this.openStatus,
+      required this.qnaNo})
+      : super(key: key);
+  final int productNo;
+  final int qnaNo;
+  final String title;
+  final String writer;
+  final String nickname;
+  final String questionCategory;
+  final String questionTitle;
+  final String questionContent;
+  final String answer;
+  final String answerStatus;
+  final String regDate;
+  final String updDate;
+  final bool openStatus;
+
+  @override
+  State<AnswerCompleteHistoryCard> createState() =>
+      _AnswerCompleteHistoryCardState();
+}
+
+class _AnswerCompleteHistoryCardState extends State<AnswerCompleteHistoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    DateTime parseRegDate = DateTime.parse(widget.regDate);
+    DateTime parseUpdDate = DateTime.parse(widget.updDate);
+    String regDateFormat = DateFormat('yyyy-MM-dd').format(parseRegDate);
+    String updDateFormat = DateFormat('yyyy-MM-dd').format(parseUpdDate);
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+      child: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  /// 공개여부 따라 아이콘 띄우기
+                  if (widget.openStatus)
+                    Icon(Icons.lock_open, color: Colors.grey, size: 20)
+                  else
+                    Icon(Icons.lock, color: Colors.grey, size: 20),
+                  SizedBox(width: 4.0),
+
+                  /// 답변 상태 따라 텍스트 띄우기
+                  if (widget.answerStatus == '답변 대기')
+                    const Text(
+                      '답변 대기',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  else if (widget.answerStatus == '답변 완료')
+                    const Text('답변 완료',
+                        style: TextStyle(
+                            color: Color(0xFFDAA520),
+                            fontWeight: FontWeight.bold)),
+                  Text(' | ${widget.questionCategory}'),
+                  Expanded(child: SizedBox()),
+                  Text('${widget.writer} | ',
+                      style: TextStyle(color: Colors.grey, fontSize: 12)),
+                  Text(regDateFormat,
+                      style: TextStyle(color: Colors.grey, fontSize: 12)),
+                  SizedBox(width: 4.0),
+                ],
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Q.',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2F4F4F)),
+                  ),
+                  SizedBox(width: 10.0),
+                  Container(
+                    width: 320.0,
+                    child: Text(
+                      widget.questionContent,
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0),
+
+              /// 답변 상태에 따라 띄우기
+              if (widget.answerStatus == '답변 완료')
+                Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'A.',
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFDAA520)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          width: 320.0,
+                          child: Text(
+                            widget.answer,
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                      ],
+                    ),
+                    SizedBox(height: 4.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text('${widget.nickname} | ',
+                            style: TextStyle(color: Colors.grey, fontSize: 12)),
+                        Text(updDateFormat,
+                            style: TextStyle(color: Colors.grey, fontSize: 12)),
+                      ],
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                        width: 300.0,
+                        child: TextButton(
+                            onPressed: () {
+                              /// 답변 삭제 요청
+                            },
+                            child: const Text('답변 삭제',
+                                style: TextStyle(
+                                    color: Color(0xFFDAA520), fontSize: 14)),
+                            style: ElevatedButton.styleFrom(
+                                side: const BorderSide(
+                                    color: Color(0xFFDAA520), width: 1.0),
+                                elevation: 0,
+                                primary: Colors.white))),
+                  ],
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
