aac1b971aad999fc1974d459e5876b08a96dc539
ANNA <109578911+joanna930224@users.noreply.github.com>
2023-01-17 19:41:33 +0900

    feat: [조안나] 판매자가 사업자 정보 등록 페이지에서 입력한 정보를 등록하도록 요청하는 API [DIY-SALES-APP-UI 123]

--- flutter/buy_idea/lib/api/seller/spring_seller_info_api.dart
+++ flutter/buy_idea/lib/api/seller/spring_seller_info_api.dart
@@ -8,6 +8,7 @@ import '../../component/seller/my_info/seller_info_data.dart';
 class SpringSellerInfoApi {
   static const String httpUri = '192.168.0.8:8888';
   static var sellerInfoResponse;
+  static var sellerInfoRegisterResponse;
 
   Future<SellerInfoData> getSellerInfo(String nickname) async {
     debugPrint('getSellerInfo()');
@@ -35,4 +36,58 @@ class SpringSellerInfoApi {
           zipcode: '');
     }
   }
+
+  sellerProfileRegister(SellerProfileRequest request) async {
+    Map addressInfoMap = {
+      'zipcode' : request.companyInfoRequest.zipcode,
+      'city' : request.companyInfoRequest.city,
+      'street' : request.companyInfoRequest.street,
+      'addressDetail' : request.companyInfoRequest.addressDetail,
+    };
+
+    var data = {
+      'nickname': request.nickname,
+      'realName': request.realName,
+      'phone': request.phone,
+      'registrationNumber': request.registrationNumber,
+      'companyInfoRequest': addressInfoMap
+    };
+
+    var body = json.encode(data);
+
+    sellerInfoRegisterResponse = await http.post(
+      Uri.http(httpUri, '/seller/profile/register'),
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+  }
+}
+
+class SellerProfileRequest {
+  String nickname;
+  String realName;
+  String phone;
+  String registrationNumber;
+  CompanyInfoRequest companyInfoRequest;
+
+  SellerProfileRequest(this.nickname, this.realName, this.phone,
+      this.registrationNumber, this.companyInfoRequest);
+
+  Map<String, dynamic> toJson() =>
+      {
+        'nickname': nickname,
+        'realName': realName,
+        'phone': phone,
+        'registrationNumber': registrationNumber,
+        'companyInfoRequest': companyInfoRequest,
+      };
+}
+
+class CompanyInfoRequest {
+  String zipcode;
+  String city;
+  String street;
+  String addressDetail;
+
+  CompanyInfoRequest(this.city, this.street, this.addressDetail, this.zipcode);
 }
