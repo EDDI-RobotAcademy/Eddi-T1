ca296afa14f91b316d7f442e232ead04d480be4d
Kim Soyeon <109579048+soyeonkim96@users.noreply.github.com>
2023-01-17 18:49:51 +0900

    style: [김소연] 판매자가 판매하려는 상품을 수정/등록할 수 있는 페이지 UI [DIY-SALES-WEB-UI 39]

--- vue/frontend/src/components/product/ProductModifyForm.vue
+++ vue/frontend/src/components/product/ProductModifyForm.vue
@@ -2,6 +2,16 @@
   <div id='content'>
     <v-form @submit.prevent="onSubmit">
 
+      <!--Seller Navigation Drawer-->
+      <seller-navi/>
+
+      <!--Top bar-->
+      <v-app-bar color="white" elevate-on-scroll height="81" app elevation="3">
+        <v-toolbar-title class="ml-3" style="color: #2F4F4F">
+          📌 Product Register
+        </v-toolbar-title>
+      </v-app-bar>
+
       <div id="title">
         <div>
           <v-layout>
@@ -191,8 +201,10 @@
 </template>
 
 <script>
+import SellerNavi from "@/components/seller/SellerNavi";
 export default {
   name: "ProductModifyForm",
+  components: {SellerNavi},
   props: {
     productNo: {
       type: String,
